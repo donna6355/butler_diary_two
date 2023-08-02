90cbf6058b631940467361c65c2b49f795213aaf
Donna <donnajun1214@gmail.com>
2023-08-02 14:54:47 +0900

    [add] assets, screens, and routes

---
+++ lib/core/constants.dart
@@ -0,0 +1,9 @@
+class NamedRoutes {
+  NamedRoutes._();
+  static const String home = '/';
+  static const String devMsg = '/dev_msg';
+  static const String diary = '/diary';
+  static const String diaryEdit = 'diary_edit';
+  static const String profile = '/profile';
+  static const String profileEdit = '/profile_edit';
+}
---
+++ lib/presentation/screen/screen.dart
@@ -0,0 +1,6 @@
+export 'dev_msg.dart';
+export 'diary_edit.dart';
+export 'diary_scr.dart';
+export 'living_room.dart';
+export 'profile_edit.dart';
+export 'profile_scr.dart';
