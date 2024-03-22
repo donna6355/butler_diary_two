79c68b8128d4b2f12c514f220761e7f8d0aae1de
Donna <donnajun1214@gmail.com>
2024-03-22 16:04:17 +0900

    [fix] full size drawer to abide admob policy

--- lib/presentation/widget/profile_drawer.dart
+++ lib/presentation/widget/profile_drawer.dart
@@ -9,39 +9,50 @@ class ProfileDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(left: 10, top: 40),
-            child: Text(
-              Lang.editProfile,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+    return SizedBox(
+      width: double.infinity,
+      child: Drawer(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(
+                    onPressed: () => Navigator.pop(context),
+                    icon: const Icon(Icons.close)),
+              ],
             ),
-          ),
-          Expanded(
-            child: ValueListenableBuilder<Box>(
-              valueListenable: Hive.box(Constants.profile).listenable(),
-              builder: (_, box, __) {
-                return box.isEmpty
-                    ? const NoCat()
-                    : ListView.builder(
-                        physics: const BouncingScrollPhysics(),
-                        itemBuilder: (_, idx) => ProfileCard(box.getAt(idx)),
-                        itemCount: box.length,
-                      );
-              },
+            const Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Text(
+                Lang.editProfile,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
             ),
-          ),
-          Center(
-            child: TextButton(
-              onPressed: () =>
-                  Navigator.of(context).pushNamed(NamedRoutes.devMsg),
-              child: const Text(Lang.devMsg),
+            Expanded(
+              child: ValueListenableBuilder<Box>(
+                valueListenable: Hive.box(Constants.profile).listenable(),
+                builder: (_, box, __) {
+                  return box.isEmpty
+                      ? const NoCat()
+                      : ListView.builder(
+                          physics: const BouncingScrollPhysics(),
+                          itemBuilder: (_, idx) => ProfileCard(box.getAt(idx)),
+                          itemCount: box.length,
+                        );
+                },
+              ),
             ),
-          ),
-        ],
+            Center(
+              child: TextButton(
+                onPressed: () =>
+                    Navigator.of(context).pushNamed(NamedRoutes.devMsg),
+                child: const Text(Lang.devMsg),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
